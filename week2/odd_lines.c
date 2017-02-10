e140b3937b12a3b108597585c0560929733b710e
Liam McCormick <ljmc2000@gmail.com>
2017-02-10 18:34:55 +0000

    odd lines now works fully

--- week2/odd_lines.c
+++ week2/odd_lines.c
@@ -14,31 +14,55 @@ Compiler: GCC 4.9.4
 
 #include <stdio.h>
 #define FILENAME 15	//max number of letters in a filename
-
+#define CHARPERLINE 75	//max number of characters per line in the input file
 int main()
 {
 	register int counter;		//counter for loops
 	FILE *file;			//pointer to the input file
 	FILE *odd,*even;		//pointers to the output files
 	char filename[FILENAME];	//recieve input for the filename
+	char line[CHARPERLINE];		//string to hold 1 line of input at a time
 
 	printf("Enter the name of the file \n");
 	gets(filename);		//input filename
 
 	file=fopen(filename,"r");		//open input file
-	even=fopen("even_lines.txt","a+");	//open output file for even lines
-	odd=fopen("odd_lines.txt","a+");	//open output file for odd lines
+	even=fopen("even_lines.txt","w+");	//open output file for even lines
+	odd=fopen("odd_lines.txt","w+");	//open output file for odd lines
 
 	if(file == NULL)
 	{
 		printf("Error, file not found \n");
+		return 1;	//exit disgracefully if file not found
 
 	}//end if
 
 	if(odd == NULL || even == NULL)
 	{
 		printf("Error opening output files \n");
+		return 1;       //exit disgracefully if the are issues with output files
 
 	}//end if
 
+	counter=0;
+
+	while(fgets(line,CHARPERLINE,file) != NULL)
+	{
+		if(counter%2==1)
+		{
+			fputs(line,odd);
+
+		}//end if
+
+		else
+		{
+			fputs(line,even);
+
+		}//end else
+
+		counter++;
+
+	}//end while
+
 }//end main
+
