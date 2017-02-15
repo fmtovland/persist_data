588f978a2722ccc77c2d273effd06287f4f69aaf
Liam Mc Cormick <ljmc2000@gmail.com>
2017-02-15 18:34:58 +0000

    added basic info to population.c

---
+++ week3/population.c
@@ -0,0 +1,16 @@
+/*
+Program Requirements:
+Download the file population.txt from webcourses/content.
+The file is a formatted text file with 3 columns: country, city and population.
+Read the file using fscanf() and create a short C program to do the following:
+
+a) Display on the screen the sum of all the population of all the cities.
+b) Display the name and the population of the city with the highest population.
+c) Create a file with all the name of the cities in Ireland.
+
+Author: Liam McCormick
+Date: 15-02-17
+OS: Gentoo
+Kernel: 4.9.9
+Compiler: 4.9.4
+*/
