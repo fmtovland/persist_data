34b965798498c31896dc4a2f4a2fcd1264e1179f
Liam Mc Cormick <ljmc2000@gmail.com>
2017-02-15 18:19:32 +0000

    all functions implemented

--- week3/merger.c
+++ week3/merger.c
@@ -13,12 +13,17 @@ Compiler: GCC 4.9.4
 
 #include <stdio.h>
 #define NUMNO 25	//max number of characters per filename
+#define CHARPERLINE 300	//max number of characters per line of files
 
 int main()
 {
+	int counter;					//for a loop
+	char buffer[CHARPERLINE];			//hold a line while copying from input to output
 	char filename1[NUMNO],filename2[NUMNO];		//strings to hold filenames
+	FILE *stage;					//pointer to which file being read from in the loop
 	FILE *file1,*file2;				//pointers to hold the files
 	FILE *output;					//pointer to output file
+
 
 	printf("Enter a filename\n");		//ask user to input filenames
 	gets(filename1);
@@ -29,11 +34,25 @@ int main()
 	file2=fopen(filename2,"r");
 	output=fopen("merged.txt","w+");
 
+
 	if(file1 == NULL || file2 == NULL || output == NULL)
 	{
 		printf("Input Output Error! \n");
 
 	}//end if
 
+	stage=file1;
+	while(counter < 2)
+	{
+		while(fgets(buffer,CHARPERLINE,stage) != NULL)
+		{
+			fputs(buffer,output);
+
+		}//end while
+
+		stage=file2;
+		counter++;
+
+	}//end while
 
 }//end main
