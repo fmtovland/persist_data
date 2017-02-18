9cf844a124c4ae014d1cba9fae5e66ac1e8f74a2
Liam McCormick <ljmc2000@gmail.com>
2017-02-18 13:32:24 +0000

    added empty program for wordsearch

---
+++ week3/wordsearch.c
@@ -0,0 +1,27 @@
+/*
+Program Requirements:
+Create a C program that asks the user the name of a text file (use gets() ), opens
+that file1 and then asks the user to input a word.
+The program will scan the file line by line and stop when it reads a line containing the
+word entered by the user.
+It will display an error message if the word is not in the file.
+
+If the word is contained in the file, the program will shows:
+a) The content of the line
+b) The line number and the position of the word in the line.
+
+
+Program Author: Liam McCormick
+Date: 18-02-17
+OS: Gentoo
+Kernel: Linux 4.9.10
+Compiler: GCC 5.4.0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+
+}//end main
