0811645c1e82f5bc9507a092c00bd8906f01077f
Liam McCormick <ljmc2000@gmail.com>
2017-03-05 17:04:49 +0000

    substructs now follow correct format

--- week5/credit_union_account.c
+++ week5/credit_union_account.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+
+struct date
+{
+	short day;	// 1 => day => 31
+	char month[4];	//can be jan, feb, mar...
+	short year;	//eg 1999
+};
+
+struct postal_address
+{
+	char number[10];	//the house name or number
+	char street[20];	//the street name
+	char city[10];		//eg. dublin
+
+};//end address
 
 struct credit_union_account
 {
-	char address[50];
-	short Date_of_birth[3];	//a small array to hold date of birth in the form dd-mm-yy
 	int pps;		//Personal Public Service number
 	char first_name[15];	//firstname
 	char surname[30];	//surname
-	int contact_number;	//phone number
+	char contact_number[10];//phone number
 	int id;			//account number
-	int balance=0;		//account balance
+	int balance;		//account balance
+
+	struct postal_address address;
+
+	struct date dob;//end dob
+
 
 };	//end struct
 
+int main()
+{
+	struct credit_union_account dit;
 
+	strcpy(dit.first_name,"Jane");
+	strcpy(dit.surname,"Ferris");
+	strcpy(dit.contact_number,"01 402300");
+	strcpy(dit.address.number,"DIT");
+	strcpy(dit.address.street,"Kevin Street");
+	strcpy(dit.address.city,"Dublin");
+
+}
