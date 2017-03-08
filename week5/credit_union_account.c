23da092746409f11a3a51e38bb0c9f671da0015b
Liam Mc Cormick <ljmc2000@gmail.com>
2017-03-08 15:47:45 +0000

    now uses malloc for credit_union_account.c

--- week5/credit_union_account.c
+++ week5/credit_union_account.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "credit_union_account.h"
 
@@ -6,18 +7,21 @@ int main()
 {
 	FILE *jane;
 	jane=fopen("jane.ant","w+");
-	struct credit_union_account dit;
+	struct credit_union_account *dit;
+	dit=malloc(sizeof(struct credit_union_account));
 
-	strcpy(dit.first_name,"Jane");
-	strcpy(dit.surname,"Ferris");
-	strcpy(dit.contact_number,"01 402300");
-	strcpy(dit.address.number,"DIT");
-	strcpy(dit.address.street,"Kevin Street");
-	strcpy(dit.address.city,"Dublin");
-	dit.dob.day=1;
-	strcpy(dit.dob.month,"jan");
-	dit.dob.year=1999;
-	strcpy(dit.pps,"123456X");
+	strcpy((*dit).first_name,"Jane");
+	strcpy((*dit).surname,"Ferris");
+	strcpy((*dit).contact_number,"01 402300");
+	strcpy((*dit).address.number,"DIT");
+	strcpy((*dit).address.street,"Kevin Street");
+	strcpy((*dit).address.city,"Dublin");
+	(*dit).dob.day=1;
+	strcpy((*dit).dob.month,"jan");
+	(*dit).dob.year=1999;
+	strcpy((*dit).pps,"123456X");
 
-	fwrite(&dit,1,sizeof(dit),jane);
+	fwrite(&dit,1,sizeof(struct credit_union_account),jane);
+
+	free(dit);
 }
