1518a2c4169f38dba4ac5aa12261a6c14b59a6e1
Liam Mc Cormick <ljmc2000@gmail.com>
2017-03-28 12:50:09 +0100

    added prototypes and edited menu

--- CA1/phonebook/main.c
+++ CA1/phonebook/main.c
@@ -3,6 +3,12 @@
 #include "lminput.h"
 #define MAX 200	//maximum number of contacts that can be stored in the phonebook
 
+//prototypes
+void addcon(struct contact*);		//add a contact
+void delcon(struct contact*);		//delete a contact
+void edcon(struct contact*);		//edit a contact
+void searchcon(struct contact*);	//search for a contact by name
+
 int main()
 {
 	struct contact phonebook[MAX];
@@ -16,6 +22,7 @@ int main()
 		printf("\n 3. Edit contact");
 		printf("\n 4. Find contact");
 		printf("\n 5. Record contacts");
+		printf("\n 0. Exit");
 
 		printf("\n");
 		scanf("%c",&input);
@@ -24,6 +31,14 @@ int main()
 
 		switch(input)
 		{
+
+			case '0':
+			{
+				printf("Have a nice day\n");
+				break;
+
+			}//end case 0
+
 			default:
 			{
 				printf("Error, invalid input\n");
