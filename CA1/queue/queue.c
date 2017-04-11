33362d6a67851a87e73c5699dd3a4290b3cbd0d8
Liam Mc Cormick <ljmc2000@gmail.com>
2017-04-11 13:59:47 +0100

    added pushing to queue also, time is now stored as 3 short ints

--- CA1/queue/queue.c
+++ CA1/queue/queue.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #include "lminput.h"
 #define NAMLEN 16	//for a name of max length 15 letters
-#define TWEET 400	//number of characters in a tweet. should be enough for a short description
+#define TWEET 401	//number of characters in a tweet (plus null). should be enough for a short description
+
+struct time	//for the time of the phonecall
+{
+	short hours;
+	short mins;
+	short secs;
+};
 
 struct queue
 {
 	char name[NAMLEN];		//name of client
 	long unsigned int contact_no;	//number client can be reached on
 	char issue[TWEET];		//short description of issue.
-	int time;			//time of phone call in seconds
+	struct time calllen;		//time of phone call in hours, minutes and seconds
 	struct queue *next;		//pointer to next person in queue
 
 };
@@ -18,10 +25,12 @@ int main()
 {
 	struct queue *in,*out;
 	char input;
+	int q_size=0;
+	int i;		//for loops
 
 	do
 	{
-		printf("Press 1 to add to queue.\n Press 2 to pop from queue.\n Press 0. to exit program.\n");
+		printf("Press 1 to add to queue.\nPress 2 to pop from queue.\nPress 0. to exit program.\n");
 		scanf("%c",&input);
 		if(overflow() != 0)
 		input='\0';
@@ -36,7 +45,37 @@ int main()
 
 			case '1':
 			{
-				//add to queue
+				//allocate element of queue
+				if(q_size == 5)
+					printf("Queue is full\n");
+				else if(q_size == 0)
+					in=out=malloc(sizeof(struct queue));
+				else
+				{
+					in->next=malloc(sizeof(struct queue));
+					in=in->next;
+				}
+
+				q_size++;
+
+				//enter information
+				printf("Enter name of customer: ");
+				wordget(in->name,NAMLEN);
+
+				printf("Enter their contact number: ");
+				scanf("%lu",&in->contact_no);
+				overflow();
+
+				printf("Enter a short description of the issue: (max %d characters)\n",TWEET-1);
+				wordget(in->issue,TWEET);
+
+				printf("enter the time of the phonecall in form hh:mm:ss\n");
+				for(i=0; i<3; i++)
+				{
+					scanf("%hd",&in->calllen.hours+i);
+					scanf("%*c");
+				}
+
 				break;
 			}
 
